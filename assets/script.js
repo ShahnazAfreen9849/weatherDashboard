4448bef15a8fc05221a71061ed23a886843bed0f
Shanzzz <shahnazafreen9849@gmail.com>
2023-04-04 23:23:39 -0400

    defined function,getWeatherData

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,48 @@
-const searchInput= document.getElementById('search');
-var searchBtn=document.getElementById('submit');
-const geoApiKey='http://api.openweathermap.org/geo/1.0/direct?q=Newark&limit=1&appid=7a02b801d5ba2290e31a45ac6b59b44a';
-const weatherapiKey= 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=7a02b801d5ba2290e31a45ac6b59b44a';
-console.log(weatherapiKey);
-console.log(searchInput);
-
-fetch(geoApiKey)
-.then(function(response){
-    console.log(response)
-    return response.json()
-})
-.then(function(data){
-    console.log(data)
-})
-searchBtn.addEventListener('click',function(){
-    console.log('click');
-
-console.log(searchInput.value);
-})
-
-
-//for history 
-// function myFunction() {
-//     window.history.back();
-//   }
+const API_KEY = "7a02b801d5ba2290e31a45ac6b59b44a";
+const searchInput = document.getElementById("search");
+const submitBtn = document.getElementById("submit-btn");
+const currentWeatherEl = document.getElementById("current-weather");
+const forecastEl = document.getElementById("forecast");
+const searchHistoryEl = document.getElementById("search-history");
+// Get the weather data for a given city name
+function getWeatherData(cityName) {
+  // Fetch the current weather data
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      // Display the current weather data
+      const currentWeather = createCurrentWeather(data);
+      currentWeatherEl.innerHTML = "";
+      currentWeatherEl.appendChild(currentWeather);
+      // Fetch the forecast weather data
+      return fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${API_KEY}&units=metric`
+      );
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      // Display the forecast weather data
+      const forecast = createForecast(data);
+      forecastEl.innerHTML = "";
+      forecastEl.appendChild(forecast);
+      // Add the city to the search history
+      addToSearchHistory(cityName);
+    })
+    .catch((error) => console.error(error));
+}
+// Create a current weather element for a given weather data object
+function createCurrentWeather(data) {
+  const { name, dt, weather, main, wind } = data;
+  const currentWeatherElement = document.createElement("div");
+  currentWeatherElement.classList.add("current-weather");
+  currentWeatherElement.innerHTML = `
+    <h2>${name} (${moment.unix(dt).format("M/D/YYYY")})</h2>
+    <img src="https://openweathermap.org/img/w/${weather[0].icon}.png" alt="${weather[0].description}" />
+    <p>Temperature: ${main.temp} °F</p>
+    <p>Humidity: ${main.humidity}%</p>
+    <p>Wind Speed: ${wind.speed} MPH</p>
+  `;
+  return currentWeatherElement;
+}
